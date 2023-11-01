b74336311d8bbe9bc92fb40aca6d06b870dfa5e5
Alice Pazitor <alice.pazitor02@e-uvt.ro>
2023-11-01 18:19:38 +0200

    implement Composite Pattern

--- src/main/java/BookPattern/Table.java
+++ src/main/java/BookPattern/Table.java
@@ -4,13 +4,31 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
-public class Table implements Element {
-    private String title;
 
-    public void print() {
-        System.out.println("Table with Title: " + this.title);
+
+public class Table extends Element {
+    
+    protected String title;
+
+    public Table(String title){
+        this.title = title;
     }
+
+    public Table(Table other){
+        this.title = other.title;
+    }
+
+    @Override 
+
+    public void print(){
+        System.out.println("Table with Title " + this.title);
+    }
+
+    @Override
+
+    public Element clone(){
+        return new Table(this);
+    }
+
+    
 }
